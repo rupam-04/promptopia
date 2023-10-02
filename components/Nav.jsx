35cc3903e2d55407bb4a0c7d6c74a3b6e14f4459
rupam-04 <rpmdey2004@gmail.com>
2023-10-02 12:16:36 +0530

    google auth done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,19 +6,19 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProvidersList = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -38,7 +38,7 @@ const Nav = () => {
 
         {/*Desktop Navigation*/}
         <div className='sm:flex hidden'>
-          {isUserLoggedIn ? (
+          {session?.user ? (
             <div className='flex gap-3 md:gap-5'>
               <Link href = "/create-prompt" className='black_btn'>
                 Create Post
@@ -48,9 +48,9 @@ const Nav = () => {
               </button>
               <Link href="/profile">
                 <Image 
-                  src = "assets/images/logo.svg"
+                  src = {session?.user?.image}
                   width={37}
-                  height={37}
+                session  height={37}
                   className='rounded-full'
                   alt='Profile'
                 />
@@ -65,7 +65,7 @@ const Nav = () => {
                   onClick={() => signIn(provider.id)}
                   className='black_btn'
                 >
-                  SignIn
+                  Sign In
                 </button>
                 ))}
             </>
@@ -74,10 +74,10 @@ const Nav = () => {
 
         {/*Mobile Navigation*/}
         <div className='sm:hidden flex relative'>
-          {isUserLoggedIn ? (
+          {session?.user ? (
             <div className='flex'>
               <Image 
-                  src = "assets/images/logo.svg"
+                  src = {session?.user?.image}
                   width={37}
                   height={37}
                   className='rounded-full'
